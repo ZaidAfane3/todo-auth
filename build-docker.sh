13fbd27d85db39f6a32b9028ed0988a0a2592507
Zaid Afaneh <afaneh.zaid98@gmail.com>
2025-10-01 00:27:17 +0300

    Upddated Build

--- build-docker.sh
+++ build-docker.sh
@@ -2,8 +2,11 @@
 
 TAG=${1:-latest}
 IMAGE_NAME="auth-service"
+gcp_image="us-central1-docker.pkg.dev/personal-473312/microservice/${IMAGE_NAME}"
 
-docker build --platform linux/x86_64 -t ${IMAGE_NAME}:linux-${TAG} .
+echo "Building and pushing auth-service images..."
+docker build --platform linux/x86_64 -t ${gcp_image}:linux-${TAG} .
 docker build --platform linux/arm64 -t ${IMAGE_NAME}:mac-${TAG} .
+docker push ${gcp_image}:linux-${TAG}
 
-
+echo "Auth service image pushed to: ${gcp_image}:linux-${TAG}"
